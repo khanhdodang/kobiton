9cfe1e240df13781f9231cd1286ecab249e74822
Khanh Do <khanhdo.pmp@gmail.com>
2020-08-21 11:54:08 +0700

    Update explaination for methods

--- src/main/java/com/kobiton/Agent.java
+++ src/main/java/com/kobiton/Agent.java
@@ -5,22 +5,62 @@ public class Agent {
     static int timeout = 120;
     static String deviceGroup = "cloud";
 
+    /**
+     * Wait for the device online with the specific UDID
+     @param hostName The default value is api.kobiton.com
+     @param username Your Username
+     @param apiKey Your API Key
+     @param deviceGroup There are cloud, private and favorite groups
+     @param udid The unique udid of the selected device
+     @param timeOutInSeconds The timeout to wait the device online
+     */
     public static boolean waitForDeviceOnline(String hostName, String username, String apiKey, String deviceGroup, String udid, int timeOutInSeconds) {
         return CheckDeviceStatus.waitForDevice(hostName, username, apiKey, deviceGroup, udid, timeOutInSeconds);
     }
 
+    /**
+     * Wait for the device online with the specific UDID
+     @param username Your Username
+     @param apiKey Your API Key
+     @param deviceGroup There are cloud, private and favorite groups
+     @param udid The unique udid of the selected device
+     @param timeOutInSeconds The timeout to wait the device online
+     */
     public static boolean waitForDeviceOnline(String username, String apiKey, String deviceGroup, String udid, int timeOutInSeconds) {
         return CheckDeviceStatus.waitForDevice(hostName, username, apiKey, deviceGroup, udid, timeOutInSeconds);
     }
 
+    /**
+     * Wait for the device online with the specific UDID
+     @param username Your Username
+     @param apiKey Your API Key
+     @param deviceGroup There are cloud, private and favorite groups
+     @param udid The unique udid of the selected device
+     Note: The default timeout 120 seconds
+     */
     public static boolean waitForDeviceOnline(String username, String apiKey, String deviceGroup, String udid) {
         return CheckDeviceStatus.waitForDevice(hostName, username, apiKey, deviceGroup, udid, timeout);
     }
 
+    /**
+     * Wait for the device online with the specific UDID
+     @param username Your Username
+     @param apiKey Your API Key
+     @param udid The unique udid of the selected device
+     @param timeOutInSeconds The timeout to wait the device online
+     Note: The default device group is cloud
+     */
     public static boolean waitForDeviceOnline(String username, String apiKey, String udid, int timeOutInSeconds) {
         return CheckDeviceStatus.waitForDevice(hostName, username, apiKey, deviceGroup, udid, timeOutInSeconds);
     }
 
+    /**
+     * Wait for the device online with the specific UDID
+     @param username Your Username
+     @param apiKey Your API Key
+     @param udid The unique udid of the selected device
+     Note: The default device group is cloud with timeout 120 seconds
+     */
     public static boolean waitForDeviceOnline(String username, String apiKey, String udid) {
         return CheckDeviceStatus.waitForDevice(hostName, username, apiKey, deviceGroup, udid, timeout);
     }
